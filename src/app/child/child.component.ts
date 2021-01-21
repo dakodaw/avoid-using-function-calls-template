4b93e5c3576dfcfbaa7d9042afb7b6ac2957eb46
dakodaw <dakodaw@yahoo.com>
2021-01-21 15:31:35 -0700

    Initial call count example

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-child',
-  templateUrl: './child.component.html',
-  styleUrls: ['./child.component.css']
+  selector: "app-child",
+  templateUrl: "./child.component.html",
+  styleUrls: ["./child.component.css"]
 })
 export class ChildComponent implements OnInit {
+  @Input() public firstName: string;
+  @Input() public lastName: string;
 
-  constructor() { }
+  public name: string = "";
 
-  ngOnInit() {
+  public getNameCallCheck: number = 0;
+  public initializedNameCheck: number = 0;
+
+  constructor() {}
+
+  public get nameGetter() {
+    this.getNameCallCheck += 1;
+    return `${this.firstName} ${this.lastName}`;
   }
 
+  ngOnInit() {
+    this.initializedNameCheck += 1;
+    this.name = `${this.firstName} ${this.lastName}`;
+  }
 }
